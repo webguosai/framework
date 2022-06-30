54788c3ab19d98e1f0e2046a694f2aba4a935925
Json <501807312@qq.com>
2022-06-30 15:21:56 +0800

    修正jwt类

--- src/Util/Jwt.php
+++ src/Util/Jwt.php
@@ -16,7 +16,7 @@ namespace Webguosai\Util;
 class Jwt
 {
     const ALG = 'HS256';
-    public static $key = 'DQaSgEfFaVmeuERRHNgxZtjWyREnDSNaJDRMoJyUkg7UQrO6ZvLkx6iapDkXsWmj';
+    public static $key = 'GkhFiMmJQYFeiUJ8NxXjr22tbgFXFg6IHSaQR2HK8qU3tMHFlYWBTs6gn2kN7QEq';
     public static $domain = '';
     public static function encode($data, int $exp = 3600, string $iss = null, string $aud = null)
     {
@@ -30,7 +30,7 @@ class Jwt
             "iat" => $time,
             // 定义在什么时间之前，该jwt都是不可用的
             "nbf" => $time,
-            'data' => $data
+            'data' => json_encode($data)
         ];
         if ($exp) {
             $token['exp'] = $time + $exp;
@@ -41,8 +41,8 @@ class Jwt
     public static function decode(string $jwt)
     {
         try {
-            $params = (array)\Firebase\JWT\JWT::decode($jwt, new \Firebase\JWT\Key(self::$key, self::ALG));
-            return $params['data'];
+            $params = \Firebase\JWT\JWT::decode($jwt, new \Firebase\JWT\Key(self::$key, self::ALG));
+            return json_decode($params->data, true);
         } catch (\Exception $e) {
             return null;
         }
