4a8cd131886c7adc5080fd49246c23f35374d4ee
Json <501807312@qq.com>
2022-07-13 17:14:09 +0800

    color

--- src/Util/Faker.php
+++ src/Util/Faker.php
@@ -378,4 +378,10 @@ class Faker
         return long2ip(mt_rand($ipLong[$randKey][0], $ipLong[$randKey][1]));
     }
 
+    public static function color()
+    {
+        $color = str_pad(dechex(mt_rand(0, 255)), 3, '0', STR_PAD_LEFT);
+
+        return '#' . $color[0] . $color[0] . $color[1] . $color[1] . $color[2] . $color[2];
+    }
 }
