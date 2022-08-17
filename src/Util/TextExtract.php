eb7e0eac9d2259bf714c12da41349f4e1b657e1b
a5436539 <501807312@qq.com>
2022-08-17 22:30:23 +0800

    1

---
+++ src/Util/TextExtract.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * 文本提取内容
+ */
+namespace Webguosai\Util;
+
+class TextExtract
+{
+    protected $text;
+    public function __construct($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * 姓名
+     *
+     * @return string
+     */
+    public function getName() {
+        $name = '';
+        $rex = '#我(?:叫|姓)([\x{4e00}-\x{9fa5}]{2,4})#iu';
+        if (preg_match($rex, $this->text, $mat)) {
+            $name = $mat[1];
+        }
+
+        return $name;
+    }
+
+    /**
+     * 性别
+     *
+     * @return string
+     */
+    public function getSex()
+    {
+        $sex = '';
+        if (preg_match('#(男|儿子)#i', $this->text)) {
+            $sex = '男';
+        }
+
+        if (preg_match('#女#i', $this->text)) {
+            $sex = '女';
+        }
+
+        return $sex;
+    }
+
+    /**
+     * 手机号
+     *
+     * @return string
+     */
+    public function getMobile()
+    {
+        $mobile = '';
+        $rex = '#[^a-zA-Z]*(1[3|4|5|6|7|8|9]{1}\d{9})#i';
+        if (preg_match($rex, $this->text, $mat)) {
+            $mobile = $mat[1];
+        }
+
+        return $mobile;
+    }
+
+    /**
+     * 获取微信号
+     *
+     * @return string
+     */
+    public function getWeChat()
+    {
+        $weChat = '';
+        $rex  = '#([a-zA-Z]{1}[a-zA-Z\d_-]{5,19})#i';
+        $rex2 = '#([1-9]{1}[0-9]{4,11})#i';
+
+        if (preg_match($rex, $this->text, $mat)) {
+            //匹配微信号官方格式
+            $weChat = $mat[1];
+        } elseif (preg_match($rex2, $this->text, $mat)) {
+            //匹配是否为QQ号，因为通过QQ号也能查找出微信号
+            $weChat = $mat[1];
+        }
+
+        // 如果没有识别到微信号，则使用电话号码
+//        if (!$weChat) {
+//            return $this->getMobile();
+//        }
+
+        return $weChat;
+    }
+
+}
