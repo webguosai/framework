d8e4fd99d260ab55954fb144446bf2a825fe6f78
guosai <501807312@qq.com>
2023-10-22 20:01:16 +0800

    1

--- src/Util/Regex.php
+++ src/Util/Regex.php
@@ -14,6 +14,11 @@ class Regex
         return self::filter($value, FILTER_VALIDATE_EMAIL);
     }
 
+    public static function isMobile($value)
+    {
+        return (bool)preg_match('/^1[3456789]\d{9}$/', $value);
+    }
+
     public static function filter($value, $filter)
     {
         return false !== filter_var($value, $filter);
